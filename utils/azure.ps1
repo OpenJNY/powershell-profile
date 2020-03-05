eeea340a53a9a158a42db0af89a53fdd95192ec9
Junya Yamaguchi <openjny@gmail.com>
2020-03-05 23:50:15 +0900

    Update

--- utils/azure.ps1
+++ utils/azure.ps1
@@ -7,6 +7,16 @@ function Select-MyAzSubscription {
         Get-AzSubscription | Out-GridView -PassThru | Select-AzSubscription
     }
 }
+
 Set-Alias azsub Select-MyAzSubscription
 Set-Alias azlogin Login-AzAccount
 Set-Alias azexit Logout-AzAccount
+
+function Select-AzPSObject ($PSObject) {
+    if ((test fzf) && (test jq)) {
+        return $PSObject | ConvertTo-Json | jq '.[] | {Name: .Name, ResourceGroupname: .ResourceGroupName}' -c | fzf | ConvertFrom-Json
+    }
+    else {
+        return $PSObject | Out-GridView -PassThru
+    }
+}
