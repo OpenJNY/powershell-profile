966f2bf82825644f7db37d99a8acbc063dba7be0
Junya Yamaguchi <openjny@gmail.com>
2020-05-13 19:53:09 +0900

    Add tf-clean

--- utils/alias.ps1
+++ utils/alias.ps1
@@ -4,3 +4,8 @@ function settings { Start-Process ms-setttings: }
 
 # rm alternative
 Set-Alias trash Remove-ItemSafely
+
+function tf-clean {
+    if (Test-Path "./terraform.tfstate") { Remove-Item "./terraform.tfstate" }
+    if (Test-Path "./.terraform" ) { Remove-Item -Path "./.terraform" -Recurse }
+}
